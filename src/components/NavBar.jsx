828e66b3a21716471d5f5d77afe3ac20a56e2708
Dev2 <>
2023-03-20 14:53:22 -0500

    Responsive menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function NavBar(){
+    const navRef = useRef();
+    
+    const resNavbar = ()=>{
+        navRef.current.classList.toggle("active");
+    } 
     return(
         <React.Fragment>
             <header>
                <div className="wrapper container_header">
                     <h1>W.</h1>
                         <nav>
-                            <ul>
+                            <ul ref={navRef}>
+                                <button className="nav-menu__close-btn" onClick={resNavbar}>
+                                    <img src="./src/assets/images/icon-menu-close.svg" alt="menu"/>
+                                </button>
                                 <li><a href="#">Home</a></li>
                                 <li><a href="#">New</a></li>
                                 <li><a href="#">Popular</a></li>
@@ -15,6 +23,9 @@ function NavBar(){
                                 <li><a href="#">Categories</a></li>
                             </ul>
                     </nav>
+                    <button className="nav-menu__btn" onClick={resNavbar}>
+                        <img src="./src/assets/images/menu.svg" alt="menu" />
+                    </button>
                </div>
             </header>
         </React.Fragment>
